3e19e09372bece0b562e04f40894f9dea4f7ee9c
UJie87 <ujie.tsai@gmail.com>
2025-08-29 08:54:47 +0000

    failed

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,12 @@ import requests
 st.set_page_config(page_title = "TPC power info", layout= "wide")
 
 st.title("TPC power information: EnelX internal use")
+st.write("test")
+
 
 # ===path setting ===
+PARQUET_PATH = Path("data/clean.parquet")
+assert PARQUET_PATH.exists(), f"can't find the file: {PARQUET_PATH.resolve()}"
+
+
 
